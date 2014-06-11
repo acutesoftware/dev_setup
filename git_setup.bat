0a0626a9be104a8644e4425dcf0a528b8423bc91
Duncan Murray <djmurray@acutesoftware.com.au>
2014-06-11 21:54:59 +0930

    git commands changed to show steps for already created github.com repository

--- git_setup.bat
+++ git_setup.bat
@@ -7,5 +7,7 @@ git add README.md
 git add git_setup.bat
 git add .\notepad_appdata\functionList.xml
 git commit -m "initial commit with notepad++ function list for python"
-git remote add origin https://github.com/acutesoftware/dev_setup.git
-git push -u origin master
+rem Normally, you use the command below UNLESS you already created the repo
+rem git remote add origin https://github.com/acutesoftware/dev_setup.git
+git remote set-url origin https://github.com/acutesoftware/dev_setup.git
+git push --set-upstream origin master
