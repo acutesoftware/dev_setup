1796a0eeda5e1c1681dc57f9a88dda0ca0601bc0
Duncan Murray <djmurray@gmail.com>
2017-07-03 00:05:07 +0930

    blank template for monitor script

---
+++ Raspberry_PI/logall/monitor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# monitor.py
+
+from gpiozero import LED
+from time import sleep
+
+def main():
+    print('monitoring ports..')
+ 
+ 
+     
+if __name__ == "__main__":
+    main()
