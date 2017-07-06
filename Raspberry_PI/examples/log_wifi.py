aae12c62ccbe193d22fb6b82973847f0cb12c278
Duncan Murray <djmurray@gmail.com>
2017-07-06 21:38:37 +0930

    tidy up and comment log_wifi.py

--- Raspberry_PI/examples/log_wifi.py
+++ Raspberry_PI/examples/log_wifi.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
-from wifi import Cell, Scheme
+from wifi import Cell
+from wifi import Scheme
 
 
-def main():
+def log_ssids():
+    """
+    scan for local Wifi points and log the SSID's to file
+    """
     print('Scanning for wifi access points...')
     ssids = [cell.ssid for cell in Cell.all('wlan0')]
     with open('ssids.lis', 'w') as f:
         for s in ssids:
             f.write(s + '\n')
 
-main()
+            
+if __name__ == "__main__":
+    log_ssids()
