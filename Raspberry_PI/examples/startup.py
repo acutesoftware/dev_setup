6507371c1b525507654086dfc886cb83d5df2664
Duncan Murray <djmurray@gmail.com>
2017-07-27 22:43:36 +0930

    added logging to startup script - it runs but sound doesnt play

--- Raspberry_PI/examples/startup.py
+++ Raspberry_PI/examples/startup.py
@@ -3,13 +3,23 @@
 # hat_startup.py 
 
 import subprocess
-
+import time
 
 
 def startup_main():
+    log_entry('startup_main called')
+    time.sleep(5)
     play_sound('hello')
 
+    
+    log_entry('startup_main finished')
+    
 
+    
+def log_entry(txt):
+    with open('rasp.log', 'a') as f:
+        f.write(txt + '\n')
+    
 def play_sound(wav_name):
     player = subprocess.Popen(["aplay", wav_name + ".wav"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
